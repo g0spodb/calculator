ffd3f0514392d6bec909be9e7708cad1ed21e5a1
Николаев Клементий <vapenationeron@gmail.com>
2021-06-25 13:48:59 +0300

    Создание исключений

--- calculator/Form1.cs
+++ calculator/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -33,7 +34,7 @@ namespace calculator
                     cumulatively += 90; //накопительно
                     using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                     {
-                        sw.WriteLine("День" + i + " |  Ставка = " + effective_rate * 100 + "%" + "|    %(накопительно) = " + cumulatively + "|     Сумма выплаты = " + (loan_amount + cumulatively));
+                        sw.WriteLine("День" + i + " |  Ставка = " + effective_rate * 100 + "%" + "|    %(накопительно) = " + cumulatively + "Р" +  "|     Сумма выплаты = " + (loan_amount + cumulatively) + "Р");
                     }
                     
             }
@@ -43,7 +44,7 @@ namespace calculator
                     cumulatively += 70;
                     using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                     {
-                        sw.WriteLine("День" + i + " |  Ставка = " + effective_rate * 100 + "%" + "|    %(накопительно) = " + cumulatively + "|     Сумма выплаты = " + (loan_amount + cumulatively));
+                        sw.WriteLine("День" + i + " |  Ставка = " + effective_rate * 100 + "%" + "|    %(накопительно) = " + cumulatively + "Р" + "|     Сумма выплаты = " + (loan_amount + cumulatively) + "Р");
                     }
 
                 }
@@ -53,7 +54,7 @@ namespace calculator
                     cumulatively += 60;
                     using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                     {
-                        sw.WriteLine("День" + i + "|  Ставка = " + effective_rate * 100 + "%" + "|    %(накопительно) = " + cumulatively + "|     Сумма выплаты = " + (loan_amount + cumulatively));
+                        sw.WriteLine("День" + i + "|  Ставка = " + effective_rate * 100 + "%" + "|    %(накопительно) = " + cumulatively + "Р"  + "|     Сумма выплаты = " + (loan_amount + cumulatively) + "Р");
                     }
 
                 }
@@ -62,11 +63,23 @@ namespace calculator
             effective_rate /= loan_term;
             double interest_amount = loan_amount * effective_rate * loan_term;
             double total_payout = interest_amount + loan_amount;
+            if (total_payout > loan_amount * 2.5) MessageBox.Show("размер выплаты по микрозайму не может превышать 2, 5 - кратного размера суммы займа");
             MessageBox.Show("Общая сумма выплаты " + Convert.ToString(total_payout) + " \n Сумма процентов " + interest_amount + " \n Эффективная ставка " + effective_rate*100 + "%");
         }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int amount = Convert.ToInt32(textBox1.Text);
+                if (amount > 500000) MessageBox.Show("предельный размер долговой нагрузки на одно физическое лицо не может превышать 500 тыс. руб.");
+                
+            }
+            catch{ }
         }
     }
 }
