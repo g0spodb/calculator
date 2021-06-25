306deaec3730b040ca6c3c04fc164e2cd6467027
Николаев Клементий <vapenationeron@gmail.com>
2021-06-25 14:52:09 +0300

    дебаггинг

--- calculator/Form1.cs
+++ calculator/Form1.cs
@@ -14,8 +14,8 @@ namespace calculator
 {
     public partial class Form1 : Form
     {
-        string writePath = @"hta.txt";
-        double effective_rate;//эффективная ставка
+        string writePath = @"Calculate_loan.txt";
+        double effective_stavka;//эффективная ставка
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -30,41 +30,41 @@ namespace calculator
             {
             if (i <=5)
             {
-                    effective_rate += 0.009;//эффективная ставка
+                    effective_stavka += 0.009;//эффективная ставка
                     nakopitelno += 90; //накопительно
                     using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                     {
-                        sw.WriteLine("День" + i + " |  Ставка = " + effective_rate * 100 + "%" + "|    %(накопительно) = " + nakopitelno + "Р" +  "|     Сумма выплаты = " + (loan_summa + nakopitelno) + "Р");
+                        sw.WriteLine("День" + i + " |  Ставка = " + effective_stavka * 100 + "%" + "|    %(накопительно) = " + nakopitelno + "Р" +  "|     Сумма выплаты = " + (loan_summa + nakopitelno) + "Р");
                     }
                     
             }
             if (i >5 & i<= 10)
             {
-                    effective_rate += 0.007;
+                    effective_stavka += 0.007;
                     nakopitelno += 70;
                     using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                     {
-                        sw.WriteLine("День" + i + " |  Ставка = " + effective_rate * 100 + "%" + "|    %(накопительно) = " + nakopitelno + "Р" + "|     Сумма выплаты = " + (loan_summa + nakopitelno) + "Р");
+                        sw.WriteLine("День" + i + " |  Ставка = " + effective_stavka * 100 + "%" + "|    %(накопительно) = " + nakopitelno + "Р" + "|     Сумма выплаты = " + (loan_summa + nakopitelno) + "Р");
                     }
 
                 }
             if (i > 10)
             {
-                    effective_rate += 0.006;
+                    effective_stavka += 0.006;
                     nakopitelno += 60;
                     using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                     {
-                        sw.WriteLine("День" + i + "|  Ставка = " + effective_rate * 100 + "%" + "|    %(накопительно) = " + nakopitelno + "Р"  + "|     Сумма выплаты = " + (loan_summa + nakopitelno) + "Р");
+                        sw.WriteLine("День" + i + "|  Ставка = " + effective_stavka * 100 + "%" + "|    %(накопительно) = " + nakopitelno + "Р"  + "|     Сумма выплаты = " + (loan_summa + nakopitelno) + "Р");
                     }
 
                 }
             
             }
-            effective_rate /= loan_srok;
-            double procent_amount = loan_summa * effective_rate * loan_srok;//Сумма процентов
+            effective_stavka /= loan_srok;
+            double procent_amount = loan_summa * effective_stavka * loan_srok;//Сумма процентов
             double total_payout = procent_amount + loan_summa;//общая сумма выплаты
             if (total_payout > loan_summa * 2.5) MessageBox.Show("размер выплаты по микрозайму не может превышать 2, 5 - кратного размера суммы займа");
-            MessageBox.Show("Общая сумма выплаты " + Convert.ToString(total_payout) + " \n Сумма процентов " + procent_amount + " \n Эффективная ставка " + effective_rate*100 + "%");
+            MessageBox.Show("Общая сумма выплаты " + Convert.ToString(total_payout) + " \n Сумма процентов " + procent_amount + " \n Эффективная ставка " + effective_stavka*100 + "%");
         }
         public Form1()
         {
@@ -76,7 +76,7 @@ namespace calculator
             try
             {
                 int amount = Convert.ToInt32(textBox1.Text);
-                if (amount > 500000) MessageBox.Show("предельный размер долговой нагрузки на одно физическое лицо не может превышать 500 тыс. руб.");
+                if (amount > 500000 || amount < 0) MessageBox.Show("предельный размер долговой нагрузки на одно физическое лицо не может превышать 500 тыс. руб.");
                 
             }
             catch{ }
